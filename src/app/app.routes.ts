bc1137c6f84dc100fa5144a842e63ef775e7c536
danielfontz <danielfontz056@gmail.com>
2026-03-24 20:40:31 -0300

    cart page commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/account/login/login.component';
+import { CartComponent } from './pages/cart/cart.component';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // First route to load
     { path: 'home', component: HomeComponent, title: 'Home' },
-    { path: 'login', component: LoginComponent, title: 'Login'}
+    { path: 'login', component: LoginComponent, title: 'Login'},
+    { path: 'carrinho', component: CartComponent, title: 'Carrinho' },
+    { path: '**', redirectTo: 'home' } // Last route to load, for 404 not found
 ];
