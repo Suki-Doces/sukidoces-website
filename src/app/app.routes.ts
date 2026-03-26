0b5243e10378c9c928f93fec2081c518b56289a8
danielfontz <danielfontz056@gmail.com>
2026-03-26 08:41:07 -0300

    profile client config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/account/login/login.component';
+import { DataEditComponent } from './pages/account/data-edit/data-edit.component';
 import { CartComponent } from './pages/cart/cart.component';
 
 
@@ -8,6 +9,7 @@ export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' }, // First route to load
     { path: 'home', component: HomeComponent, title: 'Home' },
     { path: 'login', component: LoginComponent, title: 'Login'},
+    { path: 'editar-dados', component: DataEditComponent, title: 'DadosPessoais'},
     { path: 'carrinho', component: CartComponent, title: 'Carrinho' },
     { path: '**', redirectTo: 'home' } // Last route to load, for 404 not found
 ];
--- src/app/pages/account/data-edit/data-edit.component.ts
+++ src/app/pages/account/data-edit/data-edit.component.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-data-edit',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './data-edit.component.html',
+  styleUrl: './data-edit.component.css'
 })
-export class HeaderComponent {
+export class DataEditComponent {
   isLoggedIn: boolean = true;
 }
