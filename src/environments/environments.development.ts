5214a13f0811ca48c3c30057c71c0cc4b1df7058
Daniel Fuentes <danielfontz056@gmail.com>
2026-04-29 09:30:54 -0300

    Update environments.development.ts

--- src/environments/environments.development.ts
+++ src/environments/environments.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://suki-doces-api.onrender.com/suki-doces', // <- Endereço da sua API Node.js hospedada no Render
-  productImgUrl: 'https://suki-doces-api.onrender.com/imagens/' // <- URL base para as imagens dos produtos
+  apiUrl: 'example-host/suki-doces', // <- Endereço da sua API Node.js hospedada no Render
+  productImgUrl: 'example-host/imagens/' // <- URL base para as imagens dos produtos
 };
