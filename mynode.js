7dae03bf5d647cab6ec8cb986da1c9c43b339556
daniefontz <danielfontz056@gmail.com>
2026-04-30 09:13:22 -0300

    commit

--- mynode.js
+++ mynode.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
+const errorColor = '\x1b[1;31m\x1b[0m';
 const checkSign = '\u{2705}';
 const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
-const envFile = `export const environment = {
+const envConfigFile = `export const environment = {
+    production: true,
     apiURL: '${process.env.API_URL}',
     productImgUrl: '${process.env.PRODUCT_IMG_URL}',
 };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
-fs.writeFile(targetPath, envFile, (err) => {
+const targetPath = path.join(__dirname, './src/environments/environments.ts');
+
+const envDirectory = path.join(__dirname, 'src/environments');
+if (!fs.existsSync(envDirectory)) {
+    fs.mkdirSync(envDirectory, { recursive: true });
+}
+
+fs.writeFile(targetPath, envConfigFile, (err) => {
     if (err) {
-        console.error(err);
+        console.error(errorColor ,`Erro ao gerar o env file`, err);
         throw err;
     } else {
         console.log(successColor, `${checkSign} environment.development.ts [gerado com sucesso]`);
